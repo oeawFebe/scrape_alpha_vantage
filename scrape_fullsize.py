038c3fbfc91e3819223699367dd837f71be239e5
oeawFebe <oeawFebe@gmail.com>
2020-09-09 18:33:43 -0400

    auto

--- scrape_fullsize.py
+++ scrape_fullsize.py
@@ -5,18 +5,18 @@ import pandas as pd
 
 ts = TimeSeries(key=os.environ.get("ALPHA_VANTAGE_API_KEY"),output_format='pandas', indexing_type='date')
 
-def parse_meta(meta_data):
+def _parse_meta(meta_data):
     symbol=meta_data.get("2. Symbol")
     last_ref=meta_data.get("3. Last Refreshed").replace(" ","-").replace(":","-")
     return symbol+last_ref
 
-def scrape_fullsize(ticker_list):
+def _scrape_fullsize(ticker_list):
     cnt=0
     for ticker in ticker_list:
         cnt+=1
         try:
             df,meta_data = ts.get_intraday(ticker,interval="1min",outputsize='full')
-            filename=parse_meta(meta_data)+".p"
+            filename=_parse_meta(meta_data)+".p"
             with open(filename,"wb") as p:
                 pickle.dump(df,p)
         except Exception as e:
